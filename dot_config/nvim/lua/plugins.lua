9a5940f08ae80ecfb131940494d7ca4b130080f1
Branden Akana <branden.akana@gmail.com>
2025-12-09 20:08:46 -1000

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -524,6 +524,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        gdscript = { 'gdformat' },
         -- qml = { 'qmlformat' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
