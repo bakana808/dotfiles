898b83af1fa2f8b2f26c736a17b150846ce6201b
Branden Akana <branden.akana@gmail.com>
2025-12-24 23:51:56 -1000

    Update .config/hypr/hyprland/execs.conf Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -915,4 +915,14 @@ return {
       },
     },
   },
+  { -- markdown preview
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_browser = '/usr/bin/firefox'
+    end,
+    ft = { 'markdown' },
+  },
 }
