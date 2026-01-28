4a425b137a80d73d73d5447ac25d98067a4d2fe4
Branden Akana <branden.akana@gmail.com>
2026-01-27 20:14:56 -1000

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -810,15 +810,25 @@ return {
       { '<leader>e', ':e %:h<cr>', mode = 'n', desc = 'Open File [E]xplorer (oil.nvim)' },
     },
   },
+  -- session management
+  -- {
+  --   'olimorris/persisted.nvim',
+  --   config = function()
+  --     require('persisted').setup {
+  --       autoload = true,
+  --     }
+  --   end,
+  -- },
   {
-    'olimorris/persisted.nvim',
+    'Shatur/neovim-session-manager',
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      require('persisted').setup {
-        autoload = true,
+      local config = require('session_manager.config')
+      require('session_manager').setup {
+        autoload_mode = { config.AutoloadMode.CurrentDir, config.AutoloadMode.LastSession },
       }
     end,
   },
-
   {
     'Makaze/watch.nvim',
     cmd = { 'WatchStart', 'WatchStop', 'WatchFile' },
