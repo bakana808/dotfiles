1fb5c98222e7cce3dbf16ab6783dca0b178dca1a
Branden Akana <branden.akana@gmail.com>
2026-02-24 19:37:40 -1000

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -819,15 +819,43 @@ return {
   --     }
   --   end,
   -- },
+  -- {
+  --   'Shatur/neovim-session-manager',
+  --   dependencies = { 'nvim-lua/plenary.nvim' },
+  --   config = function()
+  --     local config = require 'session_manager.config'
+  --     require('session_manager').setup {
+  --       autoload_mode = { config.AutoloadMode.GitSession, config.AutoloadMode.CurrentDir },
+  --     }
+  --   end,
+  -- },
   {
-    'Shatur/neovim-session-manager',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local config = require('session_manager.config')
-      require('session_manager').setup {
-        autoload_mode = { config.AutoloadMode.CurrentDir, config.AutoloadMode.LastSession },
-      }
+    'coffebar/neovim-project',
+    opts = {
+      projects = { -- define project roots
+        '/media/abra/code/*',
+        '~/.config/*',
+      },
+      picker = {
+        type = 'telescope', -- one of "telescope", "fzf-lua", or "snacks"
+      },
+    },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append 'globals' -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
     end,
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      -- optional picker
+      { 'nvim-telescope/telescope.nvim', tag = '0.1.4' },
+      -- optional picker
+      -- { "ibhagwan/fzf-lua" },
+      -- optional picker
+      -- { "folke/snacks.nvim" },
+      { 'Shatur/neovim-session-manager' },
+    },
+    lazy = false,
+    priority = 100,
   },
   {
     'Makaze/watch.nvim',
